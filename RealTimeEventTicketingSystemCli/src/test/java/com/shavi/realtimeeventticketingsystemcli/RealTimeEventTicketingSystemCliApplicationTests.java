4c12e738970bba17e7730018bdc06a47b4566497
ShavindiSanthupamaLiyanage <shavis1113@gmail.com>
2024-11-03 10:49:30 +0530

    Basic project structure

---
+++ RealTimeEventTicketingSystemCli/src/test/java/com/shavi/realtimeeventticketingsystemcli/RealTimeEventTicketingSystemCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shavi.realtimeeventticketingsystemcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeEventTicketingSystemCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
